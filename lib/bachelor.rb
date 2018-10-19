8e31779f5df9bdbd902d69b037286f97123c9cb8
anik1606 <anik1606@gmail.com>
2018-10-19 04:30:49 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,14 @@
+require "pry"
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_num, ppl_arr|
+    if season_num == season 
+      ppl_arr.each do |person_hash|
+        if person_hash[:status] == "Winner"
+          return person_hash[:name].split(" ").first
+        end
+    end
+  end
+  end
 end
 
 def get_contestant_name(data, occupation)
